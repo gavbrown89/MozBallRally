91a85f7169a2af220b56f7ae7f961bb4ec6043ed
gavbrown89 <glbrown2@uclan.ac.uk>
2018-03-14 17:44:59 +0000

    Website files updated

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -3,29 +3,29 @@
 session_start();
 
 if (isset($_POST['submit'])) {
-    include 'includes/dbc.inc.php';
+    include 'dbc.inc.php';
     $dbc = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
 
-    $user_name = mysqli_real_escape_string($dbc, $_POST['user_id']);
-    $password = mysqli_real_escape_string($dbc, $_POST['password']);
+    $user_id = mysqli_real_escape_string($dbc, $_POST['user_id']);
+    $pass = mysqli_real_escape_string($dbc, $_POST['password']);
 
     //Check for errors
     //Check for empty input fields
-    if (empty($user_name) || empty($password)) {
-        header("Location: index.php?login=empty");
+    if (empty($user_id) || empty($pass)) {
+        header("Location: ../index.php?login=empty");
         exit();
     } else {
-        $sql = "SELECT * FROM login_system WHERE user_name='$user_name'";
+        $sql = "SELECT * FROM login_system WHERE user_name='$user_id'";
         $result = mysqli_query($dbc, $sql);
         $resultCheck = mysqli_num_rows($result);
         if ($resultCheck < 1) {
-            header("Location: index.php?login=error");
+            header("Location: ../index.php?login=error");
             exit();
         } else {
             if ($row = mysqli_fetch_assoc($result)) {
-                $hashedPassCheck = password_verify($password, $row['user_pass']);
+                $hashedPassCheck = password_verify($pass, $row['user_pass']);
                 if ($hashedPassCheck == false) {
-                    header("Location: index.php?login=cheese");
+                    header("Location: ../index.php?login=cheese");
                     exit();
                 } elseif ($hashedPassCheck == true) {
                     // Login user
@@ -34,14 +34,14 @@ if (isset($_POST['submit'])) {
                     $_SESSION['l_name'] = $row['last_name'];
                     $_SESSION['u_email'] = $row['user_email'];
                     $_SESSION['u_pass'] = $row['user_pass'];
-                    header("Location: index.php?login=success");
+                    header("Location: ../index.php?login=success");
                     exit();
                 }
             }
         }
     }
 } else {
-    header("Location: index.php?login=error");
+    header("Location: ../index.php?login=error");
     exit();
 }
 ?>
